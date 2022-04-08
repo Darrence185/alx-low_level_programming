0db49e83badb88a5dbe5a65d880ca6066901e81f
Darrence185 <99844482+Darrence185@users.noreply.github.com>
2022-04-08 12:21:14 -0700

    Fixed task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,5 +6,14 @@
  * @b: unsigned int memory size to allocate
  * Return: void pointer to malloc'd memory space
  */
-
+ 
 void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
