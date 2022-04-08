449f877fc2a1f0464f69f18548520ed2a10315df
Darrence185 <99844482+Darrence185@users.noreply.github.com>
2022-04-08 14:07:16 -0700

    task4 done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+ 
