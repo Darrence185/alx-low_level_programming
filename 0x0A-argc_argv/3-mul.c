8cb5301c47f89f4d747532f988ce228a8d1e8fc0
Darrence185 <99844482+Darrence185@users.noreply.github.com>
2022-04-08 14:04:40 -0700

    task3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
