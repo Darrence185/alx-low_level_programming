67f61e07efc372ae2f6dfef0869019a4446988c2
Darrence185 <99844482+Darrence185@users.noreply.github.com>
2022-04-18 12:36:04 -0700

    fixing main.h file

--- printf/main.h
+++ printf/main.h
@@ -25,3 +25,22 @@ typedef struct flags
  * @f: pointer to the correct printing function
  */
 typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
